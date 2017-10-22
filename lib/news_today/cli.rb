604c9d7a01ae346231304922943c3b20c8c5c9c7
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-22 17:17:37 -0400

    starting call method

--- lib/news_today/cli.rb
+++ lib/news_today/cli.rb
@@ -2,6 +2,7 @@ class NewsToday::CLI
 
   def call
     puts "Welcome to News Today! Pick a news source to view the top headlines from that network."
+    #list the networks to pick from 
   end
 
 end
