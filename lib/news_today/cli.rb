2c0a96e3d0cdf296917e12b11e0df822545fde7e
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-22 17:20:17 -0400

    added list_networks method

--- lib/news_today/cli.rb
+++ lib/news_today/cli.rb
@@ -2,7 +2,18 @@ class NewsToday::CLI
 
   def call
     puts "Welcome to News Today! Pick a news source to view the top headlines from that network."
-    #list the networks to pick from 
+    list_networks #list the networks to pick from
+  end
+
+  def list_networks
+    puts <<-DOC
+      1. NBC
+      2. CNN
+      3. FOX
+      4. ABC
+      5. MSN
+      6. NPR
+      DOC
   end
 
 end
