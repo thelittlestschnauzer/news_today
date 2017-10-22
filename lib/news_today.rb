e3277c21dd8e8ec0d03d836f1df0cf9d99450a15
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-22 17:15:26 -0400

    set up environment

--- lib/news_today.rb
+++ lib/news_today.rb
@@ -1,4 +1,5 @@
-require "news_today/version"
+require_relative "news_today/version"
+require_relative './news_today/cli'
 
 module NewsToday
   # Your code goes here...
---
+++ lib/news_today/cli.rb
@@ -0,0 +1,7 @@
+class NewsToday::CLI
+
+  def call
+    puts "Welcome to News Today! Pick a news source to view the top headlines from that network."
+  end
+
+end
