57e690a53a91d844319e216e9b736ff4b52772e7
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-22 20:07:30 -0400

    faked the information for method to see if it was working before starting the scraping mess and added exit method

--- lib/news_today/cli.rb
+++ lib/news_today/cli.rb
@@ -1,11 +1,12 @@
 class NewsToday::CLI
 
   def call
-    puts "Welcome to News Today! Pick a news source to view the top headlines from that network."
     list_networks #list the networks to pick from
+    #list_top_stories
   end
 
   def list_networks
+    puts "Welcome to News Today! Pick a news source to view the top headlines from that network."
     puts <<-DOC
       1. NBC
       2. CNN
@@ -19,5 +20,31 @@ class NewsToday::CLI
 
   def list_top_stories
     puts "Enter the number to see the stories on that network. Type 'list' to see the list of networks again or type 'exit' when you are done."
+    input = gets.strip.downcase
+
+      if input == "1"
+        puts "stories from NBC"
+      elsif input == "2"
+        puts "stories from CNN"
+      elsif input == "3"
+        puts "stories from FOX"
+      elsif input == "4"
+        puts "stories from ABC"
+      elsif input == "5"
+        puts "stories from MSN"
+      elsif input == "6"
+        puts "stories from NPR"
+      elsif input == "list"
+        list_networks
+      elsif input == "exit"
+        exit_news
+      else
+        puts "Invalid response, please type 'list' or 'exit'."
+        list_top_stories
+      end
+    end
+
+  def exit_news
+    puts "Come back for the latest news"
   end
 end
